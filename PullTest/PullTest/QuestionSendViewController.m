956b56edac9bddcf605cd901b241e7e8ecb9e44f
samku <yenchiugu@gmail.com>
2012-09-20 23:07:12 +0800

    Dropped and question you

---
+++ PullTest/PullTest/QuestionSendViewController.m
@@ -0,0 +1,58 @@
+//
+//  QuestionSendViewController.m
+//  PullTest
+//
+//  Created by Sam Ku on 9/20/12.
+//  Copyright (c) 2012 Sam Ku. All rights reserved.
+//
+
+#import "QuestionSendViewController.h"
+
+@interface QuestionSendViewController ()
+
+
+@end
+
+
+@implementation QuestionSendViewController
+
+@synthesize delegate;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+- (IBAction)ClickYesBtn:(id)sender {
+  if (self.delegate && [self.delegate respondsToSelector:@selector(ClickSendYesBtn:)]) {
+    [self.delegate ClickSendYesBtn:self];
+  }
+}
+
+- (IBAction)ClickNoBtn:(id)sender {
+  if (self.delegate && [self.delegate respondsToSelector:@selector(ClickSendNoBtn:)]) {
+    [self.delegate ClickSendNoBtn:self];
+  }
+}
+@end
